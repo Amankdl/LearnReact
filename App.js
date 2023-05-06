204375117e6151acc5e1b91c47c59ecc243eabd9
Aman Jain <amanjainkdl@gmail.com>
2023-05-06 09:18:59 +0530

    learn react

---
+++ App.js
@@ -0,0 +1,65 @@
+// To create Hello world inside h1 tag.
+// const heading = React.createElement("h1", {id : "heading"}, "Hello World !!");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+// **************************************************************************************
+
+// Create following html code - Passing siblings than we should pass third parameter as array.
+{
+  /* <div id="parent">
+  <div id="child">
+    <h1>I am in h1 tag</h1>
+    <h2>I am in h2 tag</h2>
+  </div>
+</div> */
+}
+
+// const reactElement = React.createElement(
+//   "div",
+//   { id: "parent" },
+//   React.createElement(
+//     "div",
+//     { id: "child" },
+//     [
+//         React.createElement("h1", {}, "I am in h1 tag"),
+//         React.createElement("h2", {}, "I am in h2 tag"),
+//         React.createElement("h3", {}, "I am in h3 tag"),
+//     ]
+//   )
+// );
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(reactElement);
+
+// **************************************************************************************
+
+// Create following html code - Passing siblings than we should pass third parameter as array.
+{
+  /* <div id="parent">
+    <div id="child1">
+      <h1>I am in h1 tag</h1>
+      <h2>I am in h2 tag</h2>
+    </div>
+    <div id="child2">
+      <h1>I am in h1 tag</h1>
+      <h2>I am in h2 tag</h2>
+    </div>
+  </div> */
+}
+
+const reactElement = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "I am in h1 tag"),
+    React.createElement("h2", {}, "I am in h2 tag"),
+    React.createElement("h3", {}, "I am in h3 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I am in h1 tag"),
+    React.createElement("h2", {}, "I am in h2 tag"),
+    React.createElement("h3", {}, "I am in h3 tag"),
+  ])
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(reactElement);
